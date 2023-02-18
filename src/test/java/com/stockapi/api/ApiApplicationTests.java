ddd764b546398474c39f89bda799de44ac5492b8
Kyathi Sudheer <kyathi.koilada@gmail.com>
2023-02-18 13:38:33 +0530

    intial

---
+++ src/test/java/com/stockapi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
